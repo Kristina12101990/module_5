cd5cc5c487c7e5b0b299b662040b66556b1ba576
Kristina12101990 <kristina12101990@gmail.com>
2024-12-13 14:58:00 +0700

    Update mod5_1.py

--- mod5_1.py
+++ mod5_1.py
@@ -3,17 +3,13 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
-        self.new_floor = new_floor
-        n=0
-        for i in range(new_floor):
-            n += 1
-            print(n)
-        if new_floor > self.number_of_floors or new_floor < 1:
-                print("Такого этажа нет")
-
+        if type(new_floor) != int or new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(new_floor):
+                print(i+1)
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
-
